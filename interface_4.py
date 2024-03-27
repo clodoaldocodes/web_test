753dc38233daa83d6d07ce54105c0b2887b91677
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2024-03-26 21:19:00 -0300

    multiples plots

--- interface_4.py
+++ interface_4.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
+import scipy.stats as stats
+import plotly.graph_objs as go
 
 def ccdf(data):
     sorted_data = np.sort(data)
@@ -10,6 +12,14 @@ def ccdf(data):
     return sorted_data, ccdf
 
 def main():
+    # Configuração da página
+    st.set_page_config(
+        page_title="Análise Estatística de Dados",
+        page_icon=":chart_with_upwards_trend:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
     st.title("Análise Estatística de Dados")
 
     # Carregar o arquivo CSV
@@ -17,27 +27,95 @@ def main():
     if file is not None:
         df = pd.read_csv(file)
 
-        st.write("Visualização dos dados:")
-        st.write(df)
+        # Organizando a tela
+        col1, col2 = st.columns([2, 1])  # 2/3 e 1/3
+
+        # Visualização dos dados
+        with col1:
+            st.write("Visualização dos dados:")
+            st.write(df)
         
-        # Análise estatística básica
-        st.write("Resumo Estatístico:")
-        st.write(df.describe())
+        # Resumo Estatístico
+        with col2:
+            st.write("Resumo Estatístico:")
+            st.write(df.describe())
+
+        # Organizando os gráficos lado a lado
+        col3, col4 = st.columns(2)
 
         # Boxplot
-        st.write("Boxplot:")
-        selected_column = st.selectbox("Selecione a coluna para visualizar o boxplot", df.columns)
-        fig = px.box(df, y=selected_column)
-        st.plotly_chart(fig)
+        with col3:
+            st.write("Boxplot:")
+            selected_column_boxplot = st.selectbox("Selecione a coluna para visualizar o boxplot", df.columns)
+            fig_boxplot = px.box(df, y=selected_column_boxplot)
+            st.plotly_chart(fig_boxplot, use_container_width=True)
 
         # CCDF
-        st.write("CCDF:")
-        selected_column_ccdf = st.selectbox("Selecione a coluna para visualizar o CCDF", df.columns)
-        sorted_data, ccdf_values = ccdf(df[selected_column_ccdf])
-        fig_ccdf = px.line(x=sorted_data, y=ccdf_values, labels={'x': 'Valores ordenados', 'y': 'CCDF'})
-        fig_ccdf.update_layout(title="Complementary Cumulative Distribution Function (CCDF)")
-        fig_ccdf.update_yaxes(type="log", range=[-3, 0])  # Definindo o intervalo de -3 a 0 (ou 1 a 10^-3)
-        st.plotly_chart(fig_ccdf)
+        with col4:
+            df_columns = df.columns.tolist()
+            df_columns.insert(0, "Todas as variáveis")
+
+            # Interface Streamlit
+            st.write("CCDF:")
+            selected_column_ccdf = st.selectbox("Selecione a coluna para visualizar o CCDF", df_columns)
+            fig = go.Figure()
+
+            if selected_column_ccdf == "Todas as variáveis":
+                for column in df.columns:
+                    sorted_data, ccdf_values = ccdf(df[column])
+                    fig.add_trace(go.Scatter(x=sorted_data, y=ccdf_values, mode='lines', name=f'CCDF - {column}'))
+            else:
+                sorted_data, ccdf_values = ccdf(df[selected_column_ccdf])
+                fig.add_trace(go.Scatter(x=sorted_data, y=ccdf_values, mode='lines', name='CCDF'))
+
+            fig.update_layout(title="Complementary Cumulative Distribution Function (CCDF)",
+                            xaxis_title="Valores ordenados",
+                            yaxis_title="CCDF",
+                            yaxis_type="log",
+                            yaxis_range=[-3, 0])
+
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Organizando os gráficos lado a lado
+        col5, col6 = st.columns(2)
+
+        with col5:
+            st.write("Gráfico de Correlação:")
+            corr_matrix = df.corr()
+            fig_corr = px.imshow(corr_matrix, labels=dict(color="Correlation"), x=corr_matrix.index, y=corr_matrix.columns, 
+                                    color_continuous_scale='RdBu')
+            for i in range(len(corr_matrix.index)):
+                for j in range(len(corr_matrix.columns)):
+                    fig_corr.add_trace(
+                        dict(
+                            type='scatter',
+                            x=[corr_matrix.columns[j]],
+                            y=[corr_matrix.index[i]],
+                            text=[corr_matrix.values[i, j].round(2)],
+                            mode='text',
+                            showlegend=False
+                        )
+                    )
+            fig_corr.update_traces(textfont=dict(size=8))
+            st.plotly_chart(fig_corr)
+
+        with col6:
+            st.write("Distribuição dos Dados e Função Normal Ajustada:")
+            selected_column = st.selectbox("Selecione a coluna para visualizar a distribuição", df.columns)
+            ignore_percentile = st.slider("Percentil a ser ignorado", min_value=0, max_value=100, value=0, step=1)
+            # Ignorar o percentil selecionado
+            ignored_value = np.percentile(df[selected_column], ignore_percentile)
+            filtered_data = df[df[selected_column] < ignored_value][selected_column]
+            # Plotando a distribuição dos dados
+            fig = px.histogram(filtered_data, x=selected_column, nbins=30, histnorm='probability density', 
+                               title='Distribuição dos Dados (Ignorando {}º Percentil)'.format(ignore_percentile))
+            # Ajustando a função normal
+            mean = filtered_data.mean()
+            std = filtered_data.std()
+            x = np.linspace(mean - 4*std, mean + 4*std, 1000)
+            y = stats.norm.pdf(x, mean, std)
+            fig.add_scatter(x=x, y=y, mode='lines', name='Função Normal Ajustada')
+            st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
